e7f911b016dd9c68cda4f481ad0fa71538ba434c
Emmanuel Nyachoke <achachiez@gmail.com>
2016-08-24 14:44:25 +0300

    Disable auth (#5)
    * Disable authentication
    
    * Adding node-cron

--- server.js
+++ server.js
@@ -7,18 +7,13 @@ const hapiAuthJWT = require('hapi-auth-jwt2');
 const Vision = require('vision');
 const Routes = require('./routes/routes');
 const config = require('./config');
-var dao = require('./dao');
-
+const dao = require('./dao');
+const CronJob = require('cron').CronJob;
+const mysqlDump = require('./mysql-dump');
 const server = new Hapi.Server({});
 
 const port = 8080;
 const host = '0.0.0.0';
-var people = { // our "users database"
-  1: {
-    id: 1,
-    name: 'Jen Jones'
-  }
-};
 var validate = function(decoded, request, callback) {
   var client = dao.getClientByName(decoded.name);
   // do your checks to see if the person is valid
@@ -61,6 +56,16 @@ server.register([Vision, Inert, hapiAuthJWT], function(err) {
 
   server.start(function() {
     console.log('Server running at:', server.info.uri);
+    var job = new CronJob({
+      cronTime: config.cron.dumpSchedule,
+      onTick: function() {
+        console.log('Running Dump now');
+        mysqlDump.dumpDatabase();
+      },
+      start: false,
+      timeZone: config.cron.timezone
+    });
+    job.start();
   });
 });
 
